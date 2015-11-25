30b1274e8b94c6db57d2ebd23ffd6b0f67958e5b
Eric Citaire <ecitaire@mediametrie.fr>
2015-11-25 13:40:59 +0100

    add a test for POST, re-organize test code

--- src/test/java/vc/inreach/aws/request/test/AWSSignerTest.java
+++ src/test/java/vc/inreach/aws/request/test/AWSSignerTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static java.lang.String.format;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -26,6 +25,10 @@ public class AWSSignerTest {
      * Test case given in AWS Signing Test Suite (http://docs.aws.amazon.com/general/latest/gr/signature-v4-test-suite.html)
      * (get-vanilla-query.*)
      * 
+     * GET / http/1.1
+     * Date:Mon, 09 Sep 2011 23:36:00 GMT
+     * Host:host.foo.com
+     * 
      * @throws Exception
      */
     @Test
@@ -39,30 +42,93 @@ public class AWSSignerTest {
         String region = "us-east-1";
         String service = "host";
         
-        // HTTP request
-        String host = "host.foo.com";
-        String uri = "/";
-        String method = "GET";
-        
         // Date
         Supplier<LocalDateTime> clock = () -> LocalDateTime.of(2011, 9, 9, 23, 36, 0);
         // weird date : 09 Sep 2011 is a friday, not a monday
         String date = "Mon, 09 Sep 2011 23:36:00 GMT";
-
-        // WHEN
-        // The request is signed
-        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
-        Map<String, String> queryParams = new HashMap<>();
+        
+        // HTTP request
+        String host = "host.foo.com";
+        String uri = "/";
+        String method = "GET";
+        Map<String, String> queryParams = ImmutableMap.<String, String> builder()
+                .build();
         Map<String, Object> headers = ImmutableMap.<String, Object> builder()
                 .put("Date", date)
                 .put("Host", host)
                 .build();
         Optional<byte[]> payload = Optional.absent();
+
+        // WHEN
+        // The request is signed
+        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
         Map<String, Object> signedHeaders = signer.getSignedHeaders(uri, method, queryParams, headers, payload);
 
         // THEN
         // The signature must match the expected signature
         String expectedSignature = "b27ccfbfa7df52a200ff74193ca6e32d4b48b8856fab7ebf1c595d0670a7e470";
+        String expectedAuthorizationHeader = format(
+                "AWS4-HMAC-SHA256 Credential=%s/20110909/%s/%s/aws4_request, SignedHeaders=date;host, Signature=%s",
+                awsAccessKey, region, service, expectedSignature
+                );
+        
+        TreeMap<String, Object> caseInsensitiveSignedHeaders = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        caseInsensitiveSignedHeaders.putAll(signedHeaders);
+        assertThat(caseInsensitiveSignedHeaders).containsKey("Authorization");
+        assertThat(caseInsensitiveSignedHeaders.get("Authorization")).isEqualTo(expectedAuthorizationHeader);
+        assertThat(caseInsensitiveSignedHeaders).containsKey("Host");
+        assertThat(caseInsensitiveSignedHeaders.get("Host")).isEqualTo(host);
+        assertThat(caseInsensitiveSignedHeaders).containsKey("Date");
+        assertThat(caseInsensitiveSignedHeaders.get("Date")).isEqualTo(date);
+        assertThat(caseInsensitiveSignedHeaders).doesNotContainKey("X-Amz-Date");
+    }
+    /**
+     * Test case given in AWS Signing Test Suite (http://docs.aws.amazon.com/general/latest/gr/signature-v4-test-suite.html)
+     * (get-vanilla-query.*)
+     * 
+     * POST /?foo=bar http/1.1
+     * Date:Mon, 09 Sep 2011 23:36:00 GMT
+     * Host:host.foo.com
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testPostVanillaQuery() throws Exception {
+        // GIVEN
+        // Credentials
+        String awsAccessKey = "AKIDEXAMPLE";
+        String awsSecretKey = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY";
+        AWSCredentials credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        AWSCredentialsProvider awsCredentialsProvider = new StaticCredentialsProvider(credentials);
+        String region = "us-east-1";
+        String service = "host";
+        
+        // Date
+        Supplier<LocalDateTime> clock = () -> LocalDateTime.of(2011, 9, 9, 23, 36, 0);
+        // weird date : 09 Sep 2011 is a friday, not a monday
+        String date = "Mon, 09 Sep 2011 23:36:00 GMT";
+        
+        // HTTP request
+        String host = "host.foo.com";
+        String uri = "/";
+        String method = "POST";
+        Map<String, String> queryParams = ImmutableMap.<String, String> builder()
+                .put("foo", "bar")
+                .build();
+        Map<String, Object> headers = ImmutableMap.<String, Object> builder()
+                .put("Date", date)
+                .put("Host", host)
+                .build();
+        Optional<byte[]> payload = Optional.absent();
+
+        // WHEN
+        // The request is signed
+        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
+        Map<String, Object> signedHeaders = signer.getSignedHeaders(uri, method, queryParams, headers, payload);
+
+        // THEN
+        // The signature must match the expected signature
+        String expectedSignature = "b6e3b79003ce0743a491606ba1035a804593b0efb1e20a11cba83f8c25a57a92";
         String expectedAuthorizationHeader = format(
                 "AWS4-HMAC-SHA256 Credential=%s/20110909/%s/%s/aws4_request, SignedHeaders=date;host, Signature=%s",
                 awsAccessKey, region, service, expectedSignature
@@ -90,24 +156,25 @@ public class AWSSignerTest {
         String region = "us-east-1";
         String service = "host";
         
-        // HTTP request
-        String host = "host.foo.com";
-        String uri = "/";
-        String method = "GET";
-        
         // Date
         Supplier<LocalDateTime> clock = () -> LocalDateTime.of(2011, 9, 9, 23, 36, 0);
         // weird date : 09 Sep 2011 is a friday, not a monday
         String date = "20110909T233600Z";
-
-        // WHEN
-        // The request is signed
-        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
-        Map<String, String> queryParams = new HashMap<>();
+        
+        // HTTP request
+        String host = "host.foo.com";
+        String uri = "/";
+        String method = "GET";
+        Map<String, String> queryParams = ImmutableMap.<String, String> builder()
+                .build();
         Map<String, Object> headers = ImmutableMap.<String, Object> builder()
                 .put("Host", host)
                 .build();
         Optional<byte[]> payload = Optional.absent();
+
+        // WHEN
+        // The request is signed
+        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
         Map<String, Object> signedHeaders = signer.getSignedHeaders(uri, method, queryParams, headers, payload);
 
         // THEN
@@ -141,25 +208,26 @@ public class AWSSignerTest {
         String region = "us-east-1";
         String service = "host";
         
-        // HTTP request
-        String host = "host.foo.com";
-        String uri = "/";
-        String method = "GET";
-        
         // Date
         Supplier<LocalDateTime> clock = () -> LocalDateTime.of(2011, 9, 9, 23, 36, 0);
         // weird date : 09 Sep 2011 is a friday, not a monday
         String date = "Mon, 09 Sep 2011 23:36:00 GMT";
-
-        // WHEN
-        // The request is signed
-        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
-        Map<String, String> queryParams = new HashMap<>();
+        
+        // HTTP request
+        String host = "host.foo.com";
+        String uri = "/";
+        String method = "GET";
+        Map<String, String> queryParams = ImmutableMap.<String, String> builder()
+                .build();
         Map<String, Object> headers = ImmutableMap.<String, Object> builder()
                 .put("Date", date)
                 .put("Host", host)
                 .build();
         Optional<byte[]> payload = Optional.absent();
+
+        // WHEN
+        // The request is signed
+        AWSSigner signer = new AWSSigner(awsCredentialsProvider, region, service, clock);
         Map<String, Object> signedHeaders = signer.getSignedHeaders(uri, method, queryParams, headers, payload);
 
         // THEN
